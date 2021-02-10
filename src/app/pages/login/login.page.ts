84711f20e0d1450150d47588ff1438a99aa241b7
jacksontrr <jackson.torres.1000@gmail.com>
2021-02-10 01:51:10 -0300

    Estrutura de login pronta, só falta o design

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+
+
+
+  constructor() {
+
+   }
 
   ngOnInit() {
+
   }
 
+   loginaparecer() {
+    if (document.getElementById("logindiv").style.display == "block") {
+        document.getElementById("logindiv").style.display = "none"
+    } else {
+        document.getElementById("logindiv").style.display = "block"
+    }
+  }
 }
+
