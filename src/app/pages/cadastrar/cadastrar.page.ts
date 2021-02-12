86acc6e633f295d0565cd0754cac82f4bebf6191
jacksontrr <jackson.torres.1000@gmail.com>
2021-02-12 12:48:27 -0300

    Faltando fazer o cadastro

--- src/app/pages/cadastrar/cadastrar.page.ts
+++ src/app/pages/cadastrar/cadastrar.page.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { LoadingController, NavController, ToastController} from '@ionic/angular'; //Importação feita
+import {
+  AlertController,
+  LoadingController,
+  NavController,
+  ToastController,
+} from '@ionic/angular'; //Importação feita
 
-import { Usuariocli, Usuarioprof } from '../../models/usuario.model' // Importação feita
-import { AngularFireAuth } from "@angular/fire/auth"; //Importação feita
-
+import { Usuariocli, Usuarioprof } from '../../models/usuario.model'; // Importação feita
+import { AngularFireAuth } from '@angular/fire/auth'; //Importação feita
 
 @Component({
   selector: 'app-cadastrar',
@@ -11,15 +15,15 @@ import { AngularFireAuth } from "@angular/fire/auth"; //Importação feita
   styleUrls: ['./cadastrar.page.scss'],
 })
 export class CadastrarPage implements OnInit {
-
   constructor(
-    private loadingCtrl: LoadingController,
+    private loadingCtrl: LoadingController, // Importação do load
     private toastCtrl: ToastController,
     private afAuth: AngularFireAuth,
-    private navCtrl: NavController) { }
+    private navCtrl: NavController,
+    public alertController: AlertController // Importação do alerta
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   aparecercadastro() {
     if (document.getElementById('formcadastrar').style.display == 'block') {
@@ -29,14 +33,6 @@ export class CadastrarPage implements OnInit {
     }
   }
   aparecercli() {
-    if (document.getElementById('formcadastroprof').style.display == 'block') {
-      document.getElementById('formcadastroprof').style.display = 'none';
-      document.getElementById('formcadastrocli').style.display = 'block';
-    } else {
-      document.getElementById('formcadastrocli').style.display = 'block';
-    }
-  }
-  aparecerprof() {
     if (document.getElementById('formcadastrocli').style.display == 'block') {
       document.getElementById('formcadastrocli').style.display = 'none';
       document.getElementById('formcadastroprof').style.display = 'block';
@@ -44,47 +40,57 @@ export class CadastrarPage implements OnInit {
       document.getElementById('formcadastroprof').style.display = 'block';
     }
   }
-
-
-  
-  //se for none seleciona cliente ou profissão
-
-  registrar(){
-    var divocultaprof = document.getElementById('formcadastroprof').style.display == 'none';
-    var divaparecerprof = document.getElementById('formcadastroprof').style.display == 'block';
-
-    
-    var divocultacli = document.getElementById('formcadastrocli').style.display == 'none';
-    var divaparecercli = document.getElementById('formcadastrocli').style.display == 'block';
-
-
-    if ((divocultacli == divaparecercli) && (divocultaprof == divaparecerprof)) {
-      console.log("insere um ou outro")
-    } else if (a) {
-      
+  aparecerprof() {
+    if (document.getElementById('formcadastroprof').style.display == 'block') {
+      document.getElementById('formcadastroprof').style.display = 'none';
+      document.getElementById('formcadastrocli').style.display = 'block';
+    } else {
+      document.getElementById('formcadastrocli').style.display = 'block';
     }
-    
   }
 
-    
-  
+  //se for none seleciona cliente ou profissão
 
+  async registrar() {
+    var divocultaprof =
+      document.getElementById('formcadastroprof').style.display == 'none';
+    var divaparecerprof =
+      document.getElementById('formcadastroprof').style.display == 'block';
 
+    var divocultacli =
+      document.getElementById('formcadastrocli').style.display == 'none';
+    var divaparecercli =
+      document.getElementById('formcadastrocli').style.display == 'block';
+
+    if (divocultacli == divaparecercli && divocultaprof == divaparecerprof) {
+      let alert = await this.alertController.create({
+        // Criação do alerta
+        header: 'Atanção',
+        message: 'Selecione uma das opções acima',
+        buttons: ['Voltar'],
+      });
+
+      await alert.present();
+    } else {
+      console.log('Foi para o else'); // Aqui vai ser feito a parte do cadastrar o usuario
+    }
+  }
 
   async cadastrar(usuario: Usuariocli) {
     if (this.formValidation()) {
       // mostrar loader
       let loader = await this.loadingCtrl.create({
-        message: "Por Favor espere...",
+        message: 'Por Favor espere...',
       });
       loader.present();
       try {
         // entrar com usuário e senha
-        await this.afAuth.createUserWithEmailAndPassword(usuario.email, usuario.senha)
+        await this.afAuth
+          .createUserWithEmailAndPassword(usuario.email, usuario.senha)
           .then((dados) => {
             console.log(dados);
             // redirecionar para a página home
-            this.navCtrl.navigateRoot("home");
+            this.navCtrl.navigateRoot('home');
           })
           .catch();
       } catch (e) {
@@ -95,39 +101,38 @@ export class CadastrarPage implements OnInit {
     }
   }
 
-
-
   formValidation() {
-    if (!this.usuario.email) {// !this.usuario.email
+    if (!this.usuario.email) {
+      // !this.usuario.email
       // mostrar toast message
-      this.espera("Digite seu e-mail");
+      this.espera('Digite seu e-mail');
       return false;
     }
-    if (!this.usuario.senha) { // !this.usuario.senha
+    if (!this.usuario.senha) {
+      // !this.usuario.senha
       // mostrar toast message
-      this.espera("Digite sua Senha");
+      this.espera('Digite sua Senha');
       return false;
     }
-    return true; 
+    return true;
   }
 
-  async espera(mensagem: string) { //Tela de load
+  async espera(mensagem: string) {
+    //Tela de load
     let aguarde = await this.loadingCtrl.create({
-      message: "Aguarde por favor...",
+      message: 'Aguarde por favor...',
       duration: 900,
-    });    
+    });
   }
 
-
-
-  async outrapragina() { //Tela de load
+  async outrapragina() {
+    //Tela de load
     let aguarde = await this.loadingCtrl.create({
-      message: "Aguarde por favor...",
+      message: 'Aguarde por favor...',
       duration: 900,
     });
     aguarde.present(); //Iniciar a tela de load.
     this.navCtrl.navigateRoot('login'); //Rota, a string inserido nas aspas será direcionado.
-    aguarde.present();//Encerrar a tela de load.
-    
+    aguarde.present(); //Encerrar a tela de load.
   }
 }
