c0ffc326f89a7271764816a3a5807521edfc42c1
jacksontrr <jackson.torres.1000@gmail.com>
2021-02-14 00:05:58 -0300

    Cadastro em andamento

--- src/app/pages/cadastrar/cadastrar.page.ts
+++ src/app/pages/cadastrar/cadastrar.page.ts
@@ -9,6 +9,7 @@ import {
 
 import { Usuariocli, Usuarioprof } from '../../models/usuario.model'; // Importação feita
 import { AngularFireAuth } from '@angular/fire/auth'; //Importação feita
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-cadastrar',
@@ -24,39 +25,48 @@ export class CadastrarPage implements OnInit {
     private loadingCtrl: LoadingController, // Importação do load
     private toastCtrl: ToastController,
     private afAuth: AngularFireAuth,
+    private firestore: AngularFirestore,
     private navCtrl: NavController,
     public alertController: AlertController // Importação do alerta
   ) {}
 
   ngOnInit() {}
 
-  aparecercadastro() {
-    if (document.getElementById('formcadastrar').style.display == 'block') {
-      document.getElementById('formcadastrar').style.display = 'none';
-    } else {
-      document.getElementById('formcadastrar').style.display = 'block';
-    }
-  }
+
+
+
+
+
   aparecercli() {
-    if (document.getElementById('formcadastrocli').style.display == 'block') {
-      document.getElementById('formcadastrocli').style.display = 'none';
-      document.getElementById('formcadastroprof').style.display = 'block';
+    if (document.getElementById('formcadastroprof').style.display == 'block'){
+      document.getElementById('formcadastroprof').style.display = 'none';
+      document.getElementById('botaoprof').style.display = 'none';
+      document.getElementById('formcadastrocli').style.display = 'block';
+      document.getElementById('botaocli').style.display = 'block';
     } else {
-      document.getElementById('formcadastroprof').style.display = 'block';
+      document.getElementById('memediv').style.display = 'none';
+      document.getElementById('memebotao').style.display = 'none';
+      document.getElementById('formcadastrocli').style.display = 'block';
+      document.getElementById('botaocli').style.display = 'block';
     }
   }
   aparecerprof() {
-    if (document.getElementById('formcadastroprof').style.display == 'block') {
-      document.getElementById('formcadastroprof').style.display = 'none';
-      document.getElementById('formcadastrocli').style.display = 'block';
+    if (document.getElementById('formcadastrocli').style.display == 'block'){
+      document.getElementById('formcadastrocli').style.display = 'none';
+      document.getElementById('botaocli').style.display = 'none';
+      document.getElementById('formcadastroprof').style.display = 'block';
+      document.getElementById('botaoprof').style.display = 'block';
     } else {
-      document.getElementById('formcadastrocli').style.display = 'block';
+      document.getElementById('memediv').style.display = 'none';
+      document.getElementById('memebotao').style.display = 'none';
+      document.getElementById('formcadastroprof').style.display = 'block';
+      document.getElementById('botaoprof').style.display = 'block';
     }
   }
 
   //se for none seleciona cliente ou profissão
 
-  async registrar(usuariocli: Usuariocli) {
+  async registrarcli(usuariocli: Usuariocli) {
     var divocultaprof =
       document.getElementById('formcadastroprof').style.display == 'none';
     var divaparecerprof =
@@ -76,7 +86,7 @@ export class CadastrarPage implements OnInit {
       });
 
       await alert.present();
-    } else if (this.formValidation() && divaparecercli) {
+    } else if (this.formValidationcli() && divaparecercli && this.validarFormulariocli()) {
       // mostrar loader
       let loader = await this.loadingCtrl.create({
         message: 'Por Favor espere...',
@@ -84,34 +94,143 @@ export class CadastrarPage implements OnInit {
       loader.present();
       try {
         // entrar com usuário e senha
+
         await this.afAuth
           .createUserWithEmailAndPassword(usuariocli.email, usuariocli.senha)
           .then((dados) => {
             console.log(dados);
-
-            // redirecionar para a página home
-            this.navCtrl.navigateRoot('home');
           })
           .catch();
+        await this.firestore.collection("formularioCli").add(usuariocli);
+        // redirecionar para a página home
+        this.navCtrl.navigateRoot('home');
       } catch (e) {
-        this.espera(e);
+        this.showToast(e);
       }
       loader.dismiss();
-      console.log('Foi para o else'); // Aqui vai ser feito a parte do cadastrar o usuario
+      console.log('Foi Criado com sucesso cli"'); // Aqui vai ser feito a parte do cadastrar o usuario
     }
   }
 
-  formValidation() {
+  async registrarprof(usuarioprof: Usuarioprof) {
+    var divocultaprof =
+      document.getElementById('formcadastroprof').style.display == 'none';
+    var divaparecerprof =
+      document.getElementById('formcadastroprof').style.display == 'block';
+
+    var divocultacli =
+      document.getElementById('formcadastrocli').style.display == 'none';
+    var divaparecercli =
+      document.getElementById('formcadastrocli').style.display == 'block';
+
+    if (divocultacli == divaparecercli && divocultaprof == divaparecerprof) {
+      let alert = await this.alertController.create({
+        // Criação do alerta
+        header: 'Atanção',
+        message: 'Selecione uma das opções acima',
+        buttons: ['Voltar'],
+      });
+
+      await alert.present();
+    } else if (this.formValidationprof() && divaparecercli && this.validarFormularioprof()) {
+      // mostrar loader
+      let loader = await this.loadingCtrl.create({
+        message: 'Por Favor espere...',
+      });
+      loader.present();
+      try {
+        // entrar com usuário e senha
+
+        await this.afAuth
+          .createUserWithEmailAndPassword(usuarioprof.email, usuarioprof.senha)
+          .then((dados) => {
+            console.log(dados);
+          })
+          .catch();
+        await this.firestore.collection("formularioProf").add(usuarioprof);
+        // redirecionar para a página home
+        this.navCtrl.navigateRoot('home');
+      } catch (e) {
+        this.showToast(e);
+      }
+      loader.dismiss();
+      console.log('Foi Criado com sucesso prof"'); // Aqui vai ser feito a parte do cadastrar o usuario
+    }
+  }
+
+
+  validarFormularioprof() {
+    if (!this.usuarioprf.nome) {
+      this.showToast('Digite o nome');
+      return false;
+    }
+    if (!this.usuarioprf.telefone) {
+      this.showToast('Digite o telefone');
+      return false;
+    }
+    if (!this.usuarioprf.endereco) {
+      this.showToast('Digite o endereço');
+      return false;
+    }
+    if (!this.usuarioprof.CPFCNPJ) {
+      this.showToast('Digite o CPF');
+      return false;
+    }
+    i
+    return true;
+  }
+
+
+  //
+  formValidationprof() {
+    if (!this.usuarioprf.email) {
+      !this.usuarioprf.email;
+      //   mostrar toast message
+      this.showToast('Digite seu e-mail');
+      return false;
+    }
+    if (!this.usuarioprf.senha) {
+      !this.usuarioprf.senha;
+      //   mostrar toast message
+      this.showToast('Digite sua Senha');
+      return false;
+    }
+    return true;
+  }
+
+  validarFormulariocli() {
+    if (!this.usuariocli.nome) {
+      this.showToast('Digite o nome');
+      return false;
+    }
+    if (!this.usuariocli.telefone) {
+      this.showToast('Digite o telefone');
+      return false;
+    }
+    if (!this.usuariocli.endereco) {
+      this.showToast('Digite o endereço');
+      return false;
+    }
+    if (!this.usuariocli.CPF) {
+      this.showToast('Digite o CPF');
+      return false;
+    }
+    return true;
+  }
+
+
+  //
+  formValidationcli() {
     if (!this.usuariocli.email) {
       !this.usuariocli.email;
       //   mostrar toast message
-      this.espera('Digite seu e-mail');
+      this.showToast('Digite seu e-mail');
       return false;
     }
     if (!this.usuariocli.senha) {
       !this.usuariocli.senha;
       //   mostrar toast message
-      this.espera('Digite sua Senha');
+      this.showToast('Digite sua Senha');
       return false;
     }
     return true;
@@ -123,6 +242,14 @@ export class CadastrarPage implements OnInit {
       message: 'Aguarde por favor...',
       duration: 900,
     });
+  }
+  showToast(mensagem: string) {
+    this.toastCtrl
+      .create({
+        message: mensagem,
+        duration: 900,
+      })
+      .then((toastData) => toastData.present());
   }
 
   async outrapragina() {
